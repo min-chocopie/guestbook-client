e46ba3a7b997d6f0dfaf57569cb060eee3b1f317
minchocopie <minchocopie.lee@gmail.com>
2023-01-20 08:09:54 +0900

    테스트용 simple-client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [count, setCount] = useState();
+
+  useEffect(() => {
+    fetch('/api/count/current/')
+      .then(res => res.json())
+      .then(res => setCount(res.result));
+  })
+
+  const onButtonClick = () => {
+    fetch('/api/count/')
+      .then(res => res.json())
+      .then(res => setCount(res.result));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{count}</div>
+      <button onClick={onButtonClick}>클릭</button>
     </div>
   );
 }
