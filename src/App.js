b49886579ae1bd3e11cb37854d20c6f0a70d08d3
minchocopie <minchocopie.lee@gmail.com>
2023-01-22 15:35:23 +0900

    feat: apollo client 설정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import { useEffect, useState } from 'react';
+import { ApolloProvider } from '@apollo/client';
+
+import UserData from './components/user/Main';
+import { client } from './apollo/Apollo';
 
 
 function App() {
-  const [count, setCount] = useState();
-
-  useEffect(() => {
-    fetch('/api/count/current/')
-      .then(res => res.json())
-      .then(res => setCount(res.result));
-  })
-
-  const onButtonClick = () => {
-    fetch('/api/count/')
-      .then(res => res.json())
-      .then(res => setCount(res.result));
-  }
-
   return (
-    <div>
-      <div>{count}</div>
-      <button onClick={onButtonClick}>클릭</button>
-    </div>
+    <ApolloProvider client={client}>
+      <UserData />
+    </ApolloProvider>
   );
 }
 
